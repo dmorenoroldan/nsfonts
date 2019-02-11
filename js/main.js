b15fa9632f227cd96acc28c43246e03b52ce0be6
Daniel Moreno <danimoblog@gmail.com>
2019-02-11 08:57:41 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -67,7 +67,7 @@ $('#calc').click(function() {
 
     $("#resultado").after("<div id='ejemplo'></div>");
     $('#ejemplo').css('background-image','url("http://basehold.it/i/' + (getPx(0)*1.5) + '")');
-    ¢('#ejemplo').css('background-repeat','4px '+ (getPx(0)*1.5) +'px');
+    $('#ejemplo').css('background-repeat','4px '+ (getPx(0)*1.5) +'px');
     $("#ejemplo").append("<p style='font-size:"+getPx(0)+"px;line-height:1.5;margin-top:"+(getPx(0)*1.5)+"px;margin-bottom:"+(getPx(0)*1.5)+"px;opacity:.8;'>One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin. He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections.</p><p style='font-size:"+getPx(0)+"px;line-height:1.5;margin-bottom:"+(getPx(0)*1.5)+"px;opacity:.8;'>The bedding was hardly able to cover it and seemed ready to slide off any moment. His many legs, pitifully thin compared with the size of the rest of him, waved about helplessly as he looked.</p>");
 
     fontSwitcher(fuente+"#G","p");
